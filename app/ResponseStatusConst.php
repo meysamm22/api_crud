7b378e601c372f18f8751ba84c8eb5e880296972
Meysam Zarei <mey.zarei@yahoo.com>
2018-11-08 16:44:52 +0330

    Add Response Status Const Class

---
+++ app/ResponseStatusConst.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+class ResponseStatusConst
+{
+  const OK_RESPONSE = 200;
+  const CREATED_RESPONSE = 201;
+  const NO_CONTENT_RESPONSE = 204;
+  const BAD_REQ_RESPONSE = 400;
+
+}
